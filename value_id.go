7971f1309d9e6152637a2359b8caae1f0441715f
Mike Schinkel <mike@newclarity.net>
2023-12-04 22:34:15 -0500

    Add `NewValueId()`

--- value_id.go
+++ value_id.go
@@ -15,3 +15,29 @@ type ValueId struct {
 	// reference is true for pointer, map, slice
 	reference bool
 }
+
+// NewValueId returns a comparable struct for any reflect type.
+func NewValueId(rv *reflect.Value) (id ValueId) {
+	var ptr unsafe.Pointer
+	var altId reflect.Value
+	var ref bool
+
+	switch rv.Kind() {
+	case reflect.Slice, reflect.Map, reflect.Pointer:
+		// Use unsafe.Pointer to support the GC
+		ptr = unsafe.Pointer((*rv).Pointer())
+		ref = true
+	default:
+		altId = *rv
+	}
+	var rt reflect.Type
+	if rv.IsValid() {
+		rt = (*rv).Type()
+	}
+	return ValueId{
+		pointer:   ptr,
+		Type:      rt,
+		altId:     altId,
+		reference: ref,
+	}
+}
